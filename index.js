f97b9acb48cbb3872e30d589525a7a531b927c41
max anselmo <itmanselmo@gmail.com>
2021-09-20 22:02:19 -0400

    created functions according to the test suite, assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return [drivers[0], drivers[1]];
+}
+
+
+const returnLastTwoDrivers = function(drivers){
+    let length = drivers.length;
+    return [drivers[length-2], drivers[length-1]];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return fare * num;
+    }
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(driverArray, driverFunction) {
+    if(driverFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(driverArray);
+    } else if(driverFunction === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(driverArray);
+    }
+}
